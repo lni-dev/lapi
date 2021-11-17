68617fbf285879054fe9e4c57eadb5dc016d76a7
LinusDev <einsuperlinus@gmail.com>
2021-11-17 10:55:19 +0100

    updated docs

--- src/main/java/me/linusdev/discordbotapi/api/Updatable.java
+++ src/main/java/me/linusdev/discordbotapi/api/Updatable.java
@@ -3,7 +3,7 @@ package me.linusdev.discordbotapi.api;
 import me.linusdev.data.Data;
 
 /**
- * Objects implementing this can be updated by the api
+ * Objects implementing this can be updated by {@link LApi}
  */
 public interface Updatable {
     public void updateSelfByData(Data data);
