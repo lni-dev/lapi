f1ffbd33d0f5017ecadb52f54fb9b05c7b5fda4f
LinusDev <einsuperlinus@gmail.com>
2021-11-18 23:54:39 +0100

    Progress on Queries

--- src/main/java/me/linusdev/discordbotapi/api/communication/retriever/query/GetLinkQuery.java
+++ src/main/java/me/linusdev/discordbotapi/api/communication/retriever/query/GetLinkQuery.java
@@ -5,6 +5,7 @@ import me.linusdev.discordbotapi.api.communication.exceptions.LApiException;
 import me.linusdev.discordbotapi.api.communication.lapihttprequest.Method;
 import me.linusdev.discordbotapi.api.communication.PlaceHolder;
 import me.linusdev.discordbotapi.api.communication.lapihttprequest.LApiHttpRequest;
+import me.linusdev.discordbotapi.api.objects.channel.abstracts.Thread;
 import org.jetbrains.annotations.NotNull;
 
 import static me.linusdev.discordbotapi.api.communication.DiscordApiCommunicationHelper.*;
@@ -17,7 +18,7 @@ public class GetLinkQuery implements Query{
          * Get a {@link me.linusdev.discordbotapi.api.objects.channel.abstracts.Channel channel} by ID.
          * Returns a {@link me.linusdev.discordbotapi.api.objects.channel.abstracts.Channel channel object}.
          * If the channel is a {@link me.linusdev.discordbotapi.api.objects.channel.abstracts.Thread thread},
-         * a thread member object is included in the returned result.
+         * a {@link Thread#getMember() thread member} object is included in the returned result.
          *
          * @see PlaceHolder#CHANNEL_ID
          * @see <a href="https://discord.com/developers/docs/resources/channel#get-channel" target="_top">Get Channel</a>
@@ -25,8 +26,35 @@ public class GetLinkQuery implements Query{
         GET_CHANNEL(O_DISCORD_API_VERSION_LINK + "channels/" + CHANNEL_ID),
 
         /**
+         * <p>
+         * Returns the messages for a channel. If operating on a guild channel,
+         * this endpoint requires the {@link me.linusdev.discordbotapi.api.objects.enums.Permissions#VIEW_CHANNEL VIEW_CHANNEL}
+         * permission to be present on the current user. If the current user is missing the
+         * '{@link me.linusdev.discordbotapi.api.objects.enums.Permissions#READ_MESSAGE_HISTORY READ_MESSAGE_HISTORY}' permission in the channel
+         * then this will return no messages (since they cannot read the message history).
+         * Returns an array of {@link me.linusdev.discordbotapi.api.objects.message.Message message objects} on success.
+         * </p>
+         * <br>
+         * TODO: This Requires Query String parameters (https://discord.com/developers/docs/resources/channel#get-channel-messages-query-string-params)
+         * <p>
+         *    The before, after, and around keys are mutually exclusive, only one may be passed at a time.
+         * </p>
+         * @see PlaceHolder#CHANNEL_ID
+         * @see <a href="https://discord.com/developers/docs/resources/channel#get-channel-messages" target="_top">Get Channel Messages</a>
+         * @see <a href="https://discord.com/developers/docs/resources/channel#get-channel-messages-query-string-params" target="_top">Query String Params</a>
+         */
+        GET_CHANNEL_MESSAGES(O_DISCORD_API_VERSION_LINK + "/channels/" + CHANNEL_ID + "/messages/"),
+
+        /**
+         * <p>
+         *     Returns a specific message in the channel. If operating on a guild channel,
+         *     this endpoint requires the '{@link me.linusdev.discordbotapi.api.objects.enums.Permissions#READ_MESSAGE_HISTORY READ_MESSAGE_HISTORY}'
+         *     permission to be present on the current user. Returns a {@link me.linusdev.discordbotapi.api.objects.message.Message message object} on success.
+         * </p>
+         *
          * @see PlaceHolder#CHANNEL_ID
          * @see PlaceHolder#MESSAGE_ID
+         * @see <a href="https://discord.com/developers/docs/resources/channel#get-channel-message" target="_top">Get Channel Message</a>
          */
         GET_CHANNEL_MESSAGE(O_DISCORD_API_VERSION_LINK + "/channels/" + CHANNEL_ID + "/messages/" + MESSAGE_ID)
         ;
